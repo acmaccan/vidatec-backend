cdd869f6372c408f0f62a2b1c386c88c2ee5f2f2
Andrea Maccan <acmaccan@gmail.com>
2022-02-13 22:12:58 -0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const usersRoutes = require('./users');
 const authRoutes = require('./auth');
 const tasksRoutes = require('./tasks');
 
-// router.use('/', (req, res) => {
-//   res.send('Welcome to TO-DO app.');
-// });
+router.use('/', (req, res) => {
+  res.send('Welcome to TO-DO api. To see the app please go to https://vidatec-frontend.herokuapp.com/');
+});
 
 router.use('/register', usersRoutes);
 router.use('/login', authRoutes);
